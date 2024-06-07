34c8d2cef0d4706b353c3976e64a54b62cae199d
Amjath73 <amjathsanker4@gmail.com>
2024-06-07 14:45:02 +0530

    Nav created

---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import NavigationBar from './NavigationBar'
+
+const AddStudent = () => {
+  return (
+    <div>
+        <NavigationBar/>
+      <div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Name</label>
+                        <input type="" className="form-control" />
+                        </div>
+
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">Adm no</label>
+                    <input type="" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">RollNo</label>
+                    <input type="" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">Parent Name</label>
+                    <input type="" className="form-control"/>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label htmlFor="" className="form-label">College</label>
+                    <textarea name="" id="" className="form-control"></textarea>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">Date of birth</label>
+                    <input type="date" name="" id="" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">Email</label>
+                    <input type="" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">Password</label>
+                    <input type="password" name="" id="" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">Confirm Password</label>
+                    <input type="password" name="" id="" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <button className="btn btn-success">Register</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default AddStudent
---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,122 @@
+import React from 'react'
+import NavigationBar from './NavigationBar'
+
+const ViewAll = () => {
+    return (
+        <div>
+            <NavigationBar></NavigationBar>
+            <div className="container">
+                <br></br>
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1717545600&semt=sph" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Name</h5>
+                                        <p class="card-text">college name</p>
+                                        <a href="#" class="btn btn-primary">Register</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                            <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1717545600&semt=sph" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Name</h5>
+                                        <p class="card-text">college name</p>
+                                        <a href="#" class="btn btn-primary">Register</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                            <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1717545600&semt=sph" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Name</h5>
+                                        <p class="card-text">college name</p>
+                                        <a href="#" class="btn btn-primary">Register</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                            <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1717545600&semt=sph" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Name</h5>
+                                        <p class="card-text">college name</p>
+                                        <a href="#" class="btn btn-primary">Register</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                            <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1717545600&semt=sph" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Name</h5>
+                                        <p class="card-text">college name</p>
+                                        <a href="#" class="btn btn-primary">Register</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1717545600&semt=sph" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Name</h5>
+                                        <p class="card-text">college name</p>
+                                        <a href="#" class="btn btn-primary">Register</a>
+                                    </div>
+                                </div>
+                                </div>
+                                <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                            <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1717545600&semt=sph" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Name</h5>
+                                        <p class="card-text">college name</p>
+                                        <a href="#" class="btn btn-primary">Register</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                            <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1717545600&semt=sph" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Name</h5>
+                                        <p class="card-text">college name</p>
+                                        <a href="#" class="btn btn-primary">Register</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                            <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1717545600&semt=sph" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Name</h5>
+                                        <p class="card-text">college name</p>
+                                        <a href="#" class="btn btn-primary">Register</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                            <div class="card">
+                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1717545600&semt=sph" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Name</h5>
+                                        <p class="card-text">college name</p>
+                                        <a href="#" class="btn btn-primary">Register</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewAll
